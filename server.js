56a1ee2da21942cd304a16bb64ea56cebbe20648
Mab <mabchhan@gmail.com>
2023-01-03 15:17:37 -0800

    set up GET route for notes.html

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
+// Get Route for notes.html
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
