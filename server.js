a1f3b79c2d6d089bc3062e629d89dd28b34c9799
Mab <mabchhan@gmail.com>
2023-01-03 08:15:45 -0800

    set up server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+// set up server
+const app = express();
+
+// if any routes or 3001
+const PORT = process.env.PORT || 3001;
+
+// middleware for public files
+app.use(express.static("public"));
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// GET Route for homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
